1df8a9297018c28cd161e0aca49d93da8b8f4bbf
Azraar Azward <mazraara@gmail.com>
2018-10-04 10:56:38 +0530

    initial commit

---
+++ src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="about">
+        <h1>ABOUT</h1>
+    </div>
+</template>
+<script>
+export default {
+    name: 'about ',
+    data(){
+        return {
+        }
+    },   
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Customers.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="customers">
+        <h1>CUSTOMERS</h1>
+    </div>
+</template>
+<script>
+export default {
+    name: 'customers',
+    data(){
+        return {
+        }
+    },   
+}
+</script>
+
+<style scoped>
+
+</style>
